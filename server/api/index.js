1625e3791777e13cbcff7cd6c745dbb1f6f7e59e
AbirMazumder99 <abirmazumder1999@gmail.com>
2021-07-08 21:51:32 -0400

    test

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-module.exports = (db, isAuthenticated) => {
-    router.use('/users', require('./users')(db, isAuthenticated));
-
-    return router;
-};
